f4a46adb902157ab4fb6d253daf5c4d9af1ddfdf
Dennis <d7chau@gmail.com>
2021-07-14 21:12:26 -0400

    Added game function

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   let randomNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0; //(max - min + 1)) + min
 
   if (randomNum == 0) {
     return "rock";
-  }
-  if (randomNum == 1) {
+  } else if (randomNum == 1) {
     return "paper";
-  }
-  if (randomNum == 2) {
+  } else {
     return "scissors";
   }
 }
@@ -18,35 +19,60 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection == "rock") {
     if (computerSelection == "rock") {
-      result = "Draw! Both chose Rock";
+      result = "Draw!";
     } else if (computerSelection == "paper") {
-      result = "You Lose! Paper beats Rock";
+      result = "You Lose the Round! Paper beats Rock";
     } else if (computerSelection == "scissors") {
-      result = "You Win! Rock beats Scissors";
+      result = "You Win the Round! Rock beats Scissors";
     }
   } else if (playerSelection == "paper") {
     if (computerSelection == "rock") {
-      result = "You Win! Paper beats Rock";
+      result = "You Win the Round! Paper beats Rock";
     } else if (computerSelection == "paper") {
-      result = "Draw! Both chose Paper";
+      result = "Draw!";
     } else if (computerSelection == "scissors") {
-      result = "You Lose! Scissors beats Paper";
+      result = "You Lose the Round! Scissors beats Paper";
     }
   } else if (playerSelection == "scissors") {
     if (computerSelection == "rock") {
-      result = "You Lose! Rock beats Scissors";
+      result = "You Lose the Round! Rock beats Scissors";
     } else if (computerSelection == "paper") {
-      result = "You Win! Scissors beats Paper";
+      result = "You Win the Round! Scissors beats Paper";
     } else if (computerSelection == "scissors") {
-      result = "Draw! Both chose Scissors";
+      result = "Draw!";
     }
   }
   return result;
 }
 
-const playerSelection = "roCK";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  while (playerScore != 5 && computerScore != 5) {
+    let playerSelection = prompt("Rock, Paper, or Scissors?");
+    let computerSelection = computerPlay();
+    let roundResult = playRound(playerSelection, computerSelection);
 
-computerPlay();
-// playRound();
+    console.log(roundResult);
+
+    if (roundResult.search("You Win the Round!") > -1) {
+      playerScore++;
+    } else if (roundResult.search("You Lose the Round!") > -1) {
+      computerScore++;
+    }
+    console.log("Player Score: " + playerScore);
+    console.log("Computer Score: " + computerScore);
+    console.log("\n");
+  }
+
+  if (playerScore > computerScore) {
+    console.log("You Win the Game!");
+  } else if (playerScore == computerScore) {
+    console.log("The Game ended in a draw!");
+  } else {
+    console.log("You Lose the Game!");
+  }
+
+  playerScore = 0;
+  computerScore = 0;
+}
+
+game();
