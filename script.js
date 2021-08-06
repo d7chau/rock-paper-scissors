7a736e4fde99d391b0d8d4d829fb0f72956a055f
Dennis <d7chau@gmail.com>
2021-08-06 19:47:17 -0400

    Added score and scpre text

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let playerScore = 0;
-let computerScore = 0;
+var playerScore = 0;
+var computerScore = 0;
 
 function computerPlay() {
   let randomNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0; //(max - min + 1)) + min
 
   if (randomNum == 0) {
-    return "rock";
+    return "rock"
   } else if (randomNum == 1) {
-    return "paper";
+    return "paper"
   } else {
-    return "scissors";
+    return "scissors"
   }
 }
 
 function playRound(playerSelection, computerSelection) {
-  let result = "";
+  let result = ""
   playerSelection = playerSelection.toLowerCase(); //case-insensitive
 
   if (playerSelection == "rock") {
@@ -45,34 +45,58 @@ function playRound(playerSelection, computerSelection) {
   return result;
 }
 
-function game() {
-  // while (playerScore != 5 && computerScore != 5) {
-  //   let playerSelection = prompt("Rock, Paper, or Scissors?");
-  //   let computerSelection = computerPlay();
-  //   let roundResult = playRound(playerSelection, computerSelection);
+//UI
 
-  //   console.log(roundResult);
+var scoreInfo = document.getElementById("scoreInfo"); //connecting HTML elements to JS variables
+var playerScoreText = document.getElementById("playerScore");
+var computerScoreText = document.getElementById("computerScore");
+var rockBtn = document.getElementById("rockBtn");
+var paperBtn = document.getElementById("paperBtn");
+var scissorsBtn = document.getElementById("scissorsBtn");
 
-  //   if (roundResult.search("You Win the Round!") > -1) {
-  //     playerScore++;
-  //   } else if (roundResult.search("You Lose the Round!") > -1) {
-  //     computerScore++;
-  //   }
-  //   console.log("Player Score: " + playerScore);
-  //   console.log("Computer Score: " + computerScore);
-  //   console.log("\n");
-  // }
+rockBtn.addEventListener('click', () => handleClick("rock")) //when btn clicked, run playRound and give it the player selection
+paperBtn.addEventListener('click', () => handleClick("paper"))
+scissorsBtn.addEventListener('click', () => handleClick("scissors"))
 
-  if (playerScore > computerScore) {
-    console.log("You Win the Game!");
-  } else if (playerScore == computerScore) {
-    console.log("The Game ended in a draw!");
-  } else {
-    console.log("You Lose the Game!");
+function handleClick(playerSelection){
+  if (isGameOver() == true){
+    handleGameOver()
   }
 
+  let computerSelection = computerPlay();
+  let roundResult = playRound(playerSelection, computerSelection);
+
+  if (roundResult.search("You Win the Round!") > -1) {
+    playerScore++;
+  } else if (roundResult.search("You Lose the Round!") > -1) {
+    computerScore++;
+  }
+
+  updateScoreBoard(roundResult)
+
+  if (isGameOver() == true){
+    handleGameOver()
+  }
+}
+
+function updateScoreBoard(roundResult){
+  scoreInfo.textContent = roundResult; //display round info
+  playerScoreText.textContent = playerScore; //display round score
+  computerScoreText.textContent = computerScore;
+}
+
+function isGameOver(){
+  return (playerScore == 5) || (computerScore == 5)
+}
+
+function handleGameOver(){
+  if (playerScore == 5) {
+    scoreInfo.textContent = "You Win the Game!";
+  } else if (playerScore == computerScore) {
+    scoreInfo.textContent = "The Game ended in a draw!";
+  } else {
+    scoreInfo.textContent = "You Lose the Game!";
+  }
   playerScore = 0;
   computerScore = 0;
 }
-
-game();
